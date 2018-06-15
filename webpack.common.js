2c5f620bec46ed2c6fe29f258d318010970037cd
Ewe Seong, Yeoh <eweseong.yeoh@nokia.com>
2018-06-15 19:48:24 +0800

    Setup app structure and dev plugins

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        use: ['babel-loader', 'eslint-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: './index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.json']
+  }
+};
