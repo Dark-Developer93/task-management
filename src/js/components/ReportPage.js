c4e6889239abe67e1395212c24559dfa92fa4369
Ewe Seong, Yeoh <eweseong.yeoh@nokia.com>
2018-06-16 22:49:25 +0800

    Add report module

--- src/js/components/ReportPage.js
+++ src/js/components/ReportPage.js
@@ -1,5 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Chart from 'chart.js';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const ReportPage = () => <div>ReportPage</div>;
+import { getDaysFromNow } from '../util/DateUtil';
 
-export default ReportPage;
+export class ReportPage extends Component {
+  componentDidMount() {
+    const data = this.groupTaskByCategory();
+    if (data) {
+      new Chart(this.canvasEl, {
+        type: 'polarArea',
+        data: {
+          labels: this.getTaskCategories(),
+          datasets: [
+            {
+              data,
+              backgroundColor: ['#E0E0E0', '#FFC400', '#00E676', '#FF6384'],
+              hoverBackgroundColor: ['#E0E0E0', '#FFC400', '#00E676', '#FF6384']
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: 'Tasks due in 7 days'
+          },
+          animateScale: true,
+          animateRotate: true
+        }
+      });
+    }
+  }
+
+  getTaskCategories() {
+    const { tasks } = this.props;
+    return tasks
+      .reduce((accumulator, value) => {
+        if (accumulator.indexOf(value.category) < 0) {
+          accumulator.push(value.category);
+        }
+        return accumulator;
+      }, [])
+      .sort();
+  }
+
+  groupTaskByCategory() {
+    const { tasks } = this.props;
+    const taskCategories = this.getTaskCategories();
+    return tasks.reduce((accumulator, task) => {
+      if (!task.resolved) {
+        const taskDueDateFromNow = getDaysFromNow(task.dueDate);
+        if (taskDueDateFromNow <= 7) {
+          const categoryIndex = taskCategories.indexOf(task.category);
+          accumulator[categoryIndex] = ++accumulator[categoryIndex] || 1;
+        }
+      }
+      return accumulator;
+    }, []);
+  }
+
+  render() {
+    return <canvas ref={el => (this.canvasEl = el)} width={500} height={500} />;
+  }
+}
+
+ReportPage.defaultProps = {
+  tasks: []
+};
+
+ReportPage.propTypes = {
+  tasks: PropTypes.arrayOf(PropTypes.object)
+};
+
+const mapStateToProps = state => ({ tasks: state.tasks });
+
+export default connect(
+  mapStateToProps,
+  null
+)(ReportPage);
